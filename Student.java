54b3162775451ec6a109b23f6685baca50b130c4
Tanjila Sunmoon <89378170+Moon2312@users.noreply.github.com>
2021-09-02 23:22:27 +0600

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,92 @@
+
+package object;
+
+
+public class Student {
+    public static void main(String[] args) {
+        
+    
+   Department Student1=new Department();
+   Department Student2= new Department();
+    Department Student3= new Department();
+    Department Student4= new Department();
+    Department Student5= new Department();
+   
+   Student1.name="Shibly";
+   Student1.roll=141720;
+   Student1.group="B";
+   Student1.phone=994389848;
+   
+     System.out.println("Student 1 info:" + " ");
+     
+     System.out.print("Name: "+Student1.name +"   ");
+     System.out.print("Roll: "+Student1.roll + "   ");
+     System.out.print("Group:"+Student1.group + "   ");
+     System.out.println("Phone:"+Student1.phone + "  ");
+     
+     
+   Student2.name="Shurovi";
+   Student2.roll=141721;
+   Student2.group="C";
+   Student2.phone=994349848;
+     System.out.println();
+     System.out.println("Student 2 info:");
+     
+     System.out.print("Name: "+Student2.name + "   ");
+     System.out.print("Roll: "+Student2.roll + "   ");
+     System.out.printf("Group: "+Student2.group+"  ");
+     System.out.println("Phone: "+Student2.phone + "   ");
+     
+   Student3.name="Moon";
+   Student3.roll=141722;
+   Student3.group="C";
+   Student3.phone=994349418;
+     System.out.println();
+     System.out.println("Student 3 info:");
+     
+     System.out.print("Name: "+Student3.name + "   ");
+     System.out.print("Roll: "+Student3.roll + "   ");
+     System.out.print("Group: "+Student3.group+"  ");
+     System.out.println("Phone: "+Student3.phone + "   ");
+     
+     
+   Student4.name="Ila";
+   Student4.roll=141723;
+   Student4.group="D";
+   Student4.phone=994349418;
+     System.out.println();
+     System.out.println("Student 4 info:");
+     
+     System.out.print("Name: "+Student4.name + "   ");
+     System.out.print("Roll: "+Student4.roll + "   ");
+     System.out.print("Group: "+Student4.group+"  ");
+     System.out.println("Phone: "+Student4.phone + "   ");
+     
+     
+     
+   Student5.name="Afrin";
+   Student5.roll=141725;
+   Student5.group="E";
+   Student5.phone=994349434;
+   
+     System.out.println();
+     System.out.println("Student 5 info:");
+     
+     System.out.print("Name: "+Student5.name + "   ");
+     System.out.print("Roll: "+Student5.roll + "   ");
+     System.out.print("Group: "+Student5.group+"  ");
+     System.out.print("Phone: "+Student5.phone + "   ");
+     
+     System.out.println();
+     
+     
+     
+     
+     
+     
+     
+   
+    
+}
+    
+}
